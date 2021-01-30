3dd9a9a0cd16345604dcba51ff44d00711372578
cristixndr3s <cristiandre17@gmail.com>
2021-01-30 14:30:50 -0500

    ...

--- pyLevel1/proyecto1.py
+++ pyLevel1/proyecto1.py
@@ -1,7 +1,15 @@
-print('hola mundo')
-print('como te llamas')
-name = input()
-print('esccribe el año actual?')
-aActual = input()
-print('escribe el año de nacimiento?')
-aNacer = input()
+# este script resume lo aprendido en el capitulo uno del libro
+# automate the boring stuff with python. el objetivo es calcular
+# los años que tienes actualmente solicitando dos valores el año actual
+# y el año en el naciste
+print('hola mundo¡¡¡')  # imprimiendo una cadena
+print('como te llamas?')  # imprimiendo cadena
+name = input()  # almacenando un valor en la variable name de tipo cadena
+print('esccribe el año actual?')  # imprimiendo cadena
+aActual = input()  # almacenando una valor en la variable name de tipo cadena
+print('escribe el año de nacimiento?')  # imprimiendo cadena
+aNacer = input()  # almacenando una valor en la variable name de tipo cadena
+a = int(aActual)  # convirtiendo la cadena aActual en tipo entero
+b = int(aNacer)  # convirtiendo la cadena aActual en tipo entero
+# si no se realiza la conversion de variables tipo cadena a tipo entero
+# no se puede realizar la siguiente operacion
